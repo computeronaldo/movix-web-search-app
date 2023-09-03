db34c045992e41d4119cc099607bbb06f23e99c2
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-09-03 19:54:19 +0530

    feat: completed details page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
+import VideosSection from "./videos/VideosSection";
+import Similar from "./carousels/Similar";
+import Recommendation from "./carousels/Recommendation";
 
 const Details = () => {
-  return (
-    <div>Details</div>
-  )
-}
+  const { mediaType, id } = useParams();
+  const { data: videos, loading: videosLoading } = useFetch(
+    `/${mediaType}/${id}/videos`
+  );
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
 
-export default Details
+  return (
+    <div>
+      <DetailsBanner video={videos?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={videos} loading={videosLoading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} />
+    </div>
+  );
+};
+
+export default Details;
